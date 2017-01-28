b9ccbc66fd2055c02688c9b2e74ec0747500c584
Enrico <blackfire185@gmail.com>
2017-01-28 21:30:48 +0100

    added: start activity addItemActivity

--- app/src/main/java/ek/de/privatetimeline/AddItemActivity.java
+++ app/src/main/java/ek/de/privatetimeline/AddItemActivity.java
@@ -1,7 +1,9 @@
 package ek.de.privatetimeline;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddItemActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class AddItemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.btn_save);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
